21e651df26e6d561c9e4e00317abd561afd44f21
Denton Elliott <dendo74@hotmail.com>
2024-09-18 16:14:32 -0400

    more Router logic

--- services/Router.js
+++ services/Router.js
@@ -3,13 +3,50 @@ const Router = {
     document.querySelectorAll("a.navlink").forEach(a => {
       a.addEventListener("click", event => {
         event.preventDefault();
-        console.log("Link clicked");
+        const url = event.target.getAttribute("href");
+        Router.go(url);
       });
     })
+    // Event handler for url changes
+    window.addEventListener("popstate", event => {
+      Router.go(event.state.route, false);
+    })
 
+    // Check the initial url
+    Router.go(location.pathname);
   },
   go: (route, addToHistory=true) => {
     console.log(`Going to ${route}`)
+
+    if (addToHistory) {
+      history.pushState({ route }, '', route);
+    }
+    let pageElement = null;
+    switch (route) {
+      case "/":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Menu";
+        break;
+      case "/order":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Your Order";
+        break;
+      default:
+        if (route.startsWith("/product-")) {
+          pageElement = document.createElement("h1");
+          pageElement.textContent = "Details";
+          const paramId = route.substring(route.lastIndexOf("-")+1);
+          pageElement.dataset.id = paramId;
+        }
+    }
+    if (pageElement) {
+      // document.querySelector("main").children[0].remove();
+      const cache = document.querySelector("main");
+      cache.innerHTML = "";
+      cache.appendChild(pageElement);
+      window.scrollX = 0;
+      window.scrollY = 0;
+    }
   }
 }
 
